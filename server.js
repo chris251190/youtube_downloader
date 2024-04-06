d7bba4b4942f2a8e99fce08e06f29d6620525636
Christian Lehr <CLehr@timocom.com>
2024-04-06 16:51:33 +0200

    add logs

--- server.js
+++ server.js
@@ -9,34 +9,42 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/download', (req, res) => {
-  const url = req.query.url;
-  const starttime = req.query.starttime;
-  const endtime = req.query.endtime;
-
-  const video = ytdl(url, { quality: 'highest' });
-
-  let filePath = path.join('/tmp', 'video.mp4');
-
-  if (!fs.existsSync('/tmp')) {
-    fs.mkdirSync('/tmp');
-  }
-
-  ffmpeg(video)
-    .setStartTime(starttime)
-    .setDuration(endtime - starttime)
-    .format('mp4')
-    .on('end', () => {
-      res.download(filePath, 'video.mp4', (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          fs.unlinkSync(filePath);
-        }
-      });
-    })
-    .save(filePath);
-});
-
+    const url = req.query.url;
+    const starttime = req.query.starttime;
+    const endtime = req.query.endtime;
+  
+    console.log('Received download request for URL:', url);
+    console.log('Start time:', starttime);
+    console.log('End time:', endtime);
+  
+    const video = ytdl(url, { quality: 'highest' });
+  
+    let filePath = path.join('/tmp', 'video.mp4');
+  
+    if (!fs.existsSync('/tmp')) {
+      fs.mkdirSync('/tmp');
+    }
+  
+    console.log('Starting video processing...');
+  
+    ffmpeg(video)
+      .setStartTime(starttime)
+      .setDuration(endtime - starttime)
+      .format('mp4')
+      .on('end', () => {
+        console.log('Video processing complete.');
+        res.download(filePath, 'video.mp4', (err) => {
+          if (err) {
+            console.log('Error while sending file:', err);
+          } else {
+            console.log('File sent successfully.');
+            fs.unlinkSync(filePath);
+          }
+        });
+      })
+      .save(filePath);
+  });
+  
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });

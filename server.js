72bcb6bbb76fe344c9a1b83c9ac1909f061fafe0
Christian Lehr <CLehr@timocom.com>
2024-04-06 16:22:00 +0200

    add option to give start and endtime

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static('public'));
 
-
 app.get('/download', (req, res) => {
   const url = req.query.url;
-  res.header('Content-Disposition', `attachment; filename="video.mp4"`);
-  ytdl(url, { format: 'mp4' }).pipe(res);
+  const starttime = req.query.starttime;
+  const endtime = req.query.endtime;
+
+  const video = ytdl(url, { quality: 'highest' });
+
+  ffmpeg(video)
+    .setStartTime(starttime)
+    .setDuration(endtime - starttime)
+    .format('mp4')
+    .on('end', () => {
+      res.download('video.mp4', 'video.mp4', (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          fs.unlinkSync('video.mp4');
+        }
+      });
+    })
+    .save('video.mp4');
 });
 
 app.listen(3000, () => {

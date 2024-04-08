17706a9b178d7fcb0ab5c4ddf252f3247efc1484
Christian Lehr <CLehr@timocom.com>
2024-04-08 11:27:41 +0200

    fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.static('public'));
 

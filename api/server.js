03611d8de1125ce2794b335fcd5d037c356ecc2f
Christian Lehr <CLehr@timocom.com>
2024-04-08 22:54:56 +0200

    add option to load partial videos

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/videoInfo', async (req, res) => {
   const videoId = req.query.videoId;
-  if (!videoId) {
-    return res.status(400).send('Keine Video-ID angegeben');
+  const start = "60"; // Startzeit in Sekunden, z.B. "60" für 1 Minute
+  const end = "150"; // Endzeit in Sekunden, z.B. "150" für 2:30 Minuten
+
+  if (!videoId || !start || !end) {
+    return res.status(400).send('Parameter fehlen');
   }
 
   const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
@@ -15,21 +21,31 @@ app.get('/videoInfo', async (req, res) => {
     return res.status(400).send('Ungültige Video-URL');
   }
 
-  try {
-    // Holen Sie die Videoinformationen
-    const info = await ytdl.getInfo(videoUrl);
-    // Finden Sie den besten Stream mit Audio und Video
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest', filter: 'audioandvideo' });
-    if (format.url) {
-      // Senden Sie die URL des Formats an den Client
-      res.json({ downloadUrl: format.url });
-    } else {
-      res.status(404).send('Kein passendes Videoformat gefunden');
-    }
-  } catch (err) {
-    console.error('Fehler beim Abrufen der Videoinformationen: ', err);
-    res.status(500).send('Serverfehler beim Abrufen der Videoinformationen');
-  }
+  const tempFilePath = path.join(__dirname, `temp_${videoId}.mp4`);
+  const duration = parseInt(end, 10) - parseInt(start, 10);
+
+  ffmpeg(ytdl(videoUrl, { quality: 'highestvideo', filter: 'audioandvideo'}))
+    .setStartTime(start)
+    .duration(duration)
+    .output(tempFilePath)
+    .on('end', () => {
+      res.sendFile(tempFilePath, (err) => {
+        if (err) {
+          console.error('Fehler beim Senden der Datei: ', err);
+          return;
+        }
+        fs.unlink(tempFilePath, (deleteErr) => {
+          if (deleteErr) {
+            console.error('Fehler beim Löschen der temporären Datei: ', deleteErr);
+          }
+        });
+      });
+    })
+    .on('error', (err) => {
+      console.error('Fehler beim Verarbeiten des Videos: ', err);
+      res.status(500).send('Serverfehler beim Verarbeiten des Videos');
+    })
+    .run();
 });
 
 const port = 3000;

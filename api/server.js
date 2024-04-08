35cb67a2946515f49b9eb0a0f263f13b456de2c7
Christian Lehr <CLehr@timocom.com>
2024-04-08 12:31:01 +0200

    add functionality to download video

--- api/server.js
+++ api/server.js
@@ -1,17 +1,32 @@
-const port = 3000;
 const express = require('express');
+const ytdl = require('ytdl-core');
 const app = express();
+const port = 3000;
 
 app.get('/download', (req, res) => {
-  res.send('This is the download route!');
+  // Ensure there is a query parameter 'videoId'
+  const videoId = req.query.videoId;
+  if (!videoId) {
+    return res.status(400).send('No video ID provided');
+  }
+
+  // Construct the YouTube video URL
+  const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+
+  // Validate the URL
+  if (!ytdl.validateURL(videoUrl)) {
+    return res.status(400).send('Invalid video URL');
+  }
+
+  // Set headers for the response to indicate a download
+  res.header('Content-Disposition', `attachment; filename="video-${videoId}.mp4"`);
+
+  // Use ytdl to download the video and pipe the stream to the response
+  ytdl(videoUrl, {
+    format: 'mp4'
+  }).pipe(res);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-module.exports = app;
-
-
-
-
